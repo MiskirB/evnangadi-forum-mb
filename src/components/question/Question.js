c45173fa14c8bcf5eb7b82b5269d67055031a0ca
MiskirB <156695703+MiskirB@users.noreply.github.com>
2024-07-09 23:57:20 +0300

    propTypes added on Questionsjs

--- src/components/question/Question.js
+++ src/components/question/Question.js
@@ -1,4 +1,6 @@
+
 import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import profile from "../../../src/assets/img/profile.jpg";
 import { IoIosArrowForward } from "react-icons/io";
 import { Link } from "react-router-dom";
@@ -9,9 +11,10 @@ function Question({ searchResults }) {
   const token = localStorage.getItem("token");
   const [questions, setQuestions] = useState([]);
   const [showDescription, setShowDescription] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    // Initialize questions and showDescription states
     if (searchResults.length > 0) {
       setQuestions(searchResults);
       const initialShowDescriptionState = {};
@@ -25,13 +28,15 @@ function Question({ searchResults }) {
   }, [searchResults]);
 
   async function getAllQuestions() {
+    setLoading(true);
+    setError("");
     try {
-      const data = await axios.get("/questions/all-questions", {
+      const { data } = await axios.get("/questions/all-questions", {
         headers: {
           Authorization: "Bearer " + token,
         },
       });
-      const sortedQuestions = data.data.sort((a, b) => b.id - a.id);
+      const sortedQuestions = data.sort((a, b) => b.id - a.id);
       const initialShowDescriptionState = {};
       sortedQuestions.forEach((question) => {
         initialShowDescriptionState[question.questionid] = false;
@@ -39,7 +44,10 @@ function Question({ searchResults }) {
       setShowDescription(initialShowDescriptionState);
       setQuestions(sortedQuestions);
     } catch (error) {
+      setError("Failed to load questions. Please try again later.");
       console.log(error.response);
+    } finally {
+      setLoading(false);
     }
   }
 
@@ -50,44 +58,159 @@ function Question({ searchResults }) {
     }));
   };
 
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div className="text-red-500">{error}</div>;
+
   return (
     <>
-      {questions &&
-        questions.map((question) => (
-          <div className="border-b-2" key={question.questionid}>
-            <div className="flex gap-6">
-              <div>
-                <div className="w-24">
-                  <img src={profile} alt="" />
-                </div>
-                <h2 className="text-center">{question.username}</h2>
+      {questions.map((question) => (
+        <div className="border-b-2" key={question.questionid}>
+          <div className="flex gap-6">
+            <div>
+              <div className="w-24">
+                <img src={profile} alt="Profile" />
               </div>
-              <div className="flex w-11/12 justify-between items-center">
-                <Link to={`./answer/${question.questionid}`}>
-                  <p>{question.title}</p>
-                </Link>
-                {showDescription[question.questionid] ? (
-                  <FaAngleDown
-                    onClick={() => handleDropDown(question.questionid)}
-                  />
-                ) : (
-                  <IoIosArrowForward
-                    onClick={() => handleDropDown(question.questionid)}
-                  />
-                )}
-              </div>
+              <h2 className="text-center">{question.username}</h2>
             </div>
-            {showDescription[question.questionid] && (
-              <div className="p-6">
-                <p>
-                  Description: - <span>{question.description}</span>
-                </p>
-              </div>
-            )}
+            <div className="flex w-11/12 justify-between items-center">
+              <Link to={`./answer/${question.questionid}`}>
+                <p>{question.title}</p>
+              </Link>
+              {showDescription[question.questionid] ? (
+                <FaAngleDown
+                  onClick={() => handleDropDown(question.questionid)}
+                />
+              ) : (
+                <IoIosArrowForward
+                  onClick={() => handleDropDown(question.questionid)}
+                />
+              )}
+            </div>
           </div>
-        ))}
+          {showDescription[question.questionid] && (
+            <div className="p-6">
+              <p>
+                Description: - <span>{question.description}</span>
+              </p>
+            </div>
+          )}
+        </div>
+      ))}
     </>
   );
 }
 
+Question.propTypes = {
+  searchResults: PropTypes.arrayOf(
+    PropTypes.shape({
+      questionid: PropTypes.number.isRequired,
+      username: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
 export default Question;
+
+
+
+
+
+
+
+
+
+// import React, { useEffect, useState } from "react";
+// import profile from "../../../src/assets/img/profile.jpg";
+// import { IoIosArrowForward } from "react-icons/io";
+// import { Link } from "react-router-dom";
+// import axios from "../../axiosConfig";
+// import { FaAngleDown } from "react-icons/fa6";
+
+// function Question({ searchResults }) {
+//   const token = localStorage.getItem("token");
+//   const [questions, setQuestions] = useState([]);
+//   const [showDescription, setShowDescription] = useState({});
+
+//   useEffect(() => {
+//     // Initialize questions and showDescription states
+//     if (searchResults.length > 0) {
+//       setQuestions(searchResults);
+//       const initialShowDescriptionState = {};
+//       searchResults.forEach((question) => {
+//         initialShowDescriptionState[question.questionid] = false;
+//       });
+//       setShowDescription(initialShowDescriptionState);
+//     } else {
+//       getAllQuestions();
+//     }
+//   }, [searchResults]);
+
+//   async function getAllQuestions() {
+//     try {
+//       const data = await axios.get("/questions/all-questions", {
+//         headers: {
+//           Authorization: "Bearer " + token,
+//         },
+//       });
+//       const sortedQuestions = data.data.sort((a, b) => b.id - a.id);
+//       const initialShowDescriptionState = {};
+//       sortedQuestions.forEach((question) => {
+//         initialShowDescriptionState[question.questionid] = false;
+//       });
+//       setShowDescription(initialShowDescriptionState);
+//       setQuestions(sortedQuestions);
+//     } catch (error) {
+//       console.log(error.response);
+//     }
+//   }
+
+//   const handleDropDown = (questionId) => {
+//     setShowDescription((prevState) => ({
+//       ...prevState,
+//       [questionId]: !prevState[questionId],
+//     }));
+//   };
+
+//   return (
+//     <>
+//       {questions &&
+//         questions.map((question) => (
+//           <div className="border-b-2" key={question.questionid}>
+//             <div className="flex gap-6">
+//               <div>
+//                 <div className="w-24">
+//                   <img src={profile} alt="" />
+//                 </div>
+//                 <h2 className="text-center">{question.username}</h2>
+//               </div>
+//               <div className="flex w-11/12 justify-between items-center">
+//                 <Link to={`./answer/${question.questionid}`}>
+//                   <p>{question.title}</p>
+//                 </Link>
+//                 {showDescription[question.questionid] ? (
+//                   <FaAngleDown
+//                     onClick={() => handleDropDown(question.questionid)}
+//                   />
+//                 ) : (
+//                   <IoIosArrowForward
+//                     onClick={() => handleDropDown(question.questionid)}
+//                   />
+//                 )}
+//               </div>
+//             </div>
+//             {showDescription[question.questionid] && (
+//               <div className="p-6">
+//                 <p>
+//                   Description: - <span>{question.description}</span>
+//                 </p>
+//               </div>
+//             )}
+//           </div>
+//         ))}
+//     </>
+//   );
+// }
+
+// export default Question;
