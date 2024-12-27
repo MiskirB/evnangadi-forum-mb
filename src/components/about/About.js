23acb57b2e9ccdf69a8cab3416f3300cc6da0c22
MiskirB <156695703+MiskirB@users.noreply.github.com>
2024-12-28 00:14:55 +0300

    Update About.js
    about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -15,9 +15,6 @@ export default function About() {
           <p className="text-sm mb-2 text-slate-500">
           Evangadi Forum is an innovative platform dedicated to fostering a vibrant community centered around technology and coding. Specifically designed for current, former, and prospective students of Evangadi, the forum provides a collaborative space where users can engage in tech-related Q&A discussions. This dynamic environment encourages the sharing of knowledge, resources, and experiences, helping users to deepen their understanding of various tech and coding topics while also building connections within the Evangadi community.
           </p>
-          <p className="text-sm mb-2 text-slate-500">
-          Focused on tech and coding-related queries, Evangadi Forum serves as a valuable resource for students at all stages of their learning journey. Whether seeking answers to complex coding problems, exploring new programming languages, or staying updated on the latest industry trends, users can find and contribute to a wealth of information. By offering a supportive and interactive platform, Evangadi Forum not only enhances the learning experience but also empowers students to achieve their goals and stay connected with like-minded individuals in the tech field.
-          </p>
           <div className="textcenter">
             <button className="mt-7 px-8 py-1 bg-orange-500 text-white rounded-sm mb-20 text-sm">
               HOW IT WORKS
